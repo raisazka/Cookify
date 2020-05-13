064e40cfec2f4d74062e55b283dcf48e4e6f215b
raisazka <raisazka@gmail.com>
2020-05-13 20:38:50 +0700

    Create Storyboard References

--- FnBTrainingApp/ViewController.swift
+++ FnBTrainingApp/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let role = "admin"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func login(_ sender: Any) {
+        if role == "admin" {
+            presentStoryboard(sbName: "Management", vcIdentifier: "ManagementSB")
+        } else {
+            presentStoryboard(sbName: "Employee", vcIdentifier: "EmpSB")
+        }
+    }
+    
+    func presentStoryboard(sbName : String, vcIdentifier : String) {
+         let storyboard = UIStoryboard(name: sbName, bundle: nil)
+          let vc = storyboard.instantiateViewController(withIdentifier: vcIdentifier)
+          self.show(vc, sender: self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
 
