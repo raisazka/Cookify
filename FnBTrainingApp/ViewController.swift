7ea6cace1af6c9f77049cc18d6287660a9d5a9d5
raisazka <raisazka@gmail.com>
2020-05-13 11:22:43 +0700

    initial commit

---
+++ FnBTrainingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FnBTrainingApp
+//
+//  Created by Rais on 13/05/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
