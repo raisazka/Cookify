c70b59ed4b79fc711dee7e8830682f7c705e6ac4
raisazka <raisazka@gmail.com>
2020-05-18 09:22:25 +0700

    Add Login Page + View Employee on Management

---
+++ FnBTrainingApp/View/Collection/EmployeeCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  EmployeeCollectionCell.swift
+//  FnBTrainingApp
+//
+//  Created by Rais on 17/05/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeCollectionCell: UICollectionViewCell {
+    
+}
--- FnBTrainingApp/ViewController.swift
+++ FnBTrainingApp/ViewController.swift
@@ -12,9 +12,34 @@ class ViewController: UIViewController {
 
     let role = "admin"
     
+    @IBOutlet weak var fieldEmail: UITextField!
+    @IBOutlet weak var fieldPassword: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initTextField(imageName: "envelope.fill", textField: fieldEmail)
+        initTextField(imageName: "lock.fill", textField: fieldPassword)
+        loginBtn.layer.cornerRadius = 10
+    }
+    
+    private func initTextField(imageName: String, textField: UITextField) {
+        let imageView = UIImageView(frame:
+        CGRect(x: 0, y: 5, width: 28, height: 20))
+        let iconView: UIView = UIView(frame:
+        CGRect(x: 15, y: 0, width: 40, height: 30))
         
+        let image = UIImage(systemName: imageName)
+        imageView.image = image
+        iconView.addSubview(imageView)
+        textField.leftView = iconView
+        
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: textField.frame.height - 1, width: textField.frame.width, height: 2.0)
+        bottomLine.backgroundColor = UIColor.white.cgColor
+        textField.borderStyle = UITextField.BorderStyle.none
+        textField.layer.addSublayer(bottomLine)
+        textField.textColor = UIColor.white
     }
     
     @IBAction func login(_ sender: Any) {
@@ -25,10 +50,11 @@ class ViewController: UIViewController {
         }
     }
     
-    func presentStoryboard(sbName : String, vcIdentifier : String) {
+    private func presentStoryboard(sbName : String, vcIdentifier : String) {
          let storyboard = UIStoryboard(name: sbName, bundle: nil)
           let vc = storyboard.instantiateViewController(withIdentifier: vcIdentifier)
-          self.show(vc, sender: self)
+          vc.modalPresentationStyle = .fullScreen
+          show(vc, sender: self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
