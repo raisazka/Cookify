b6e1764632a0074b6ed954d7ca3e5b3ab3073a5c
Theofani Samuel <liow.ts@gmail.com>
2020-05-21 12:21:26 +0700

    add Collection view

---
+++ FnBTrainingApp/View/Collection/TrainingCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  TrainingCollectionView.swift
+//  FnBTrainingApp
+//
+//  Created by Theofani on 21/05/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class TrainingCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FnBTrainingApp/View/Collection/TrainingCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TrainingCollectionViewCell.swift
+//  FnBTrainingApp
+//
+//  Created by Theofani on 21/05/20.
+//  Copyright © 2020 Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class TrainingCollectionViewCell: UICollectionViewCell {
+    
+}
