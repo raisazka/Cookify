9f1be4b8a0deee11f1b0877a93ee873032da071b
Theofani Samuel <liow.ts@gmail.com>
2020-05-26 16:53:09 +0700

    add Alert before delete cell

--- FnBTrainingApp/View/Management/PersiapanEditVC.swift
+++ FnBTrainingApp/View/Management/PersiapanEditVC.swift
@@ -62,12 +62,19 @@ extension PersiapanEditVC: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if (editingStyle == .delete){
-            let alert = UIAlertController(title: "Message", message: "Clicked at index\(indexPath.row)", preferredStyle: .alert)
-            let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
-            alert.addAction(action)
+            let alert = UIAlertController(title: "Warning!", message: "Hapus data ini?", preferredStyle: .alert)
+            let delete = UIAlertAction(title: "Ya", style: .destructive, handler: { action in
+                self.myModel.remove(at: indexPath.row)
+                tableView.reloadData()
+                
+            })
+            let cancel = UIAlertAction(title: "Tidak", style: .cancel, handler: { action in
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            })
+            alert.addAction(delete)
+            alert.addAction(cancel)
             self.present(alert, animated: true, completion: nil)
-            myModel.remove(at: indexPath.row)
-            tableView.reloadData()
+           
         }
     }
     
